d79e92a844c5fab5f8daef82a0dc4461eca19d4b
banana <banana@eurus.cn>
2017-08-19 11:40:55 +0800

    add example

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in schema_parser.gemspec
 gemspec
+gem 'rake'
--- lib/schema_parser.rb
+++ lib/schema_parser.rb
@@ -1,31 +1,31 @@
 require 'schema_parser/version'
 require 'schema_parser/schema_node'
 require 'schema_parser/schema_link'
-module SchemaParser
-  class String
-    def underscore
-      self.gsub(/::/, '/').
-        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-        gsub(/([a-z\d])([A-Z])/,'\1_\2').
-        tr("-", "_").
-        downcase
-    end
-  end
 
+class String
+  def underscore
+    self.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      downcase
+  end
+end
+
+module SchemaParser
   class Parser
     def initialize(source, target)
       @source, @target = source, target
       @nodes = []
-      @h = {}
+      @edges = {}
     end
 
     def split_into_nodes
-      original = File.open(@source, 'r') { |file| file.readlines }
-      blankless = original.reject{ |line| line.match(/^$/) }
-
-      @nodes = blankless
+      @nodes = File.open(@source, 'r') { |file| file.readlines }
+      .reject{ |line| line.match(/^$/) }
       .join()
-      .split("--------------------------------------------------").map do |b|
+      .split("--------------------------------------------------")
+      .map do |b|
         sources = b.lines.reject{|line| line == "\n"}
         name = sources.first.gsub("\n",'')
         attrs = sources[2..-1].map { |e| e.split(' ')
@@ -35,7 +35,7 @@ module SchemaParser
         .reject{ |s| s == "id:int(11)"}
         .concat ["#{name.underscore}_id:int(11)"]
         .uniq
-        sn = SchemaNode.new(name, attrs)
+        SchemaNode.new(name, attrs)
       end
     end
 
@@ -51,10 +51,10 @@ module SchemaParser
 
         @nodes.combination(2).to_a.each do |node_a, node_b|
           link = SchemaLink.new(node_a, node_b)
-          @h["#{node_a.name}_#{node_b.name}"] = link.link?
+          @edges["#{node_a.name}_#{node_b.name}"] = link.link?
         end
 
-        @h.select {|k,v|v == true}.each do |k,v|
+        @edges.select {|k,v|v == true}.each do |k,v|
           writeable.write "#{k.split('_')[0]} -> #{k.split('_')[1]} [dir=\"none\"]\n"
         end
 
